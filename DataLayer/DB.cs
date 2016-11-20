bf1ca48c62ad6f23c97292ea9bd96bc0381e91aa
Alen Hujdur <hujduralen@gmail.com>
2016-11-20 20:11:25 +0100

    ok

--- DataLayer/DB.cs
+++ DataLayer/DB.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,31 @@ namespace DataLayer
 {
     public class DB
     {
+        public static string ConnectionString
+        {
+            get
+            {
+
+                string connStr = ConfigurationManager.ConnectionStrings["AWConnection"].ToString();
+                SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder(connStr);
+                sb.ApplicationName = ApplicationName ?? sb.ApplicationName;
+                sb.ConnectTimeout = (ConnectionTimeout > 0) ? ConnectionTimeout : sb.ConnectTimeout;
+                return sb.ToString();
+            }
+        }
+        public static SqlConnection GetSqlConnection()
+        {
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            conn.Open();
+            return conn;
+        }
+        /// <summary>
+        public static int ConnectionTimeout { get; set; }
+        /// </summary>
+        public static string ApplicationName
+        {
+            get;
+            set;
+        }
     }
 }
--- WinDemo/Form1.cs
+++ WinDemo/Form1.cs
@@ -16,5 +16,10 @@ namespace WinDemo
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
